d1fd033ee595fc859066a1fcab03dad45aabfbcc
Cloud Drake Games <92053275+HireathGames@users.noreply.github.com>
2025-10-23 15:08:21 -0500

    Added Classes and Updated Main.
    Added the first class, the Point class, and updated Main's code to fit the point class.

---
+++ Classes.py
@@ -0,0 +1,34 @@
+class Point:
+    #ID represents if a ship is present and what type it is.
+    #state refers to whether it unknown, a hit or a miss. 
+    #The destroyed state (-2) is only visual and is functionally the same as a miss.
+    def __init__(self, _ID = 0, _State = 0):
+        self.ID = _ID
+        self.state = _State
+    def __str__(self):
+        output = str(self.ID) + ", " + str(self.state)
+        return(output)
+    def __repr__(self):
+        output = str(self.state)
+        return(output)
+    #Basic getters and setters
+    def get_ID(self):
+        return(self.ID)
+    def get_State(self):
+        return(self.state)
+    def set_ID(self, input):
+        self.ID = input
+    def set_State(self, input):
+        self.state = input
+    #This is code for when the player selects a target, the sets it to it's proper state and returns if it hit or missed.
+    def chosen_target(self):
+        if self.state == 0:
+            if self.ID == 0:
+                self.state = -1
+                return("Miss!")
+            else:
+                self.state = 1
+                return("Hit!")
+                
+        else:
+            return("INVALID TARGET")
--- Main.py
+++ Main.py
@@ -1,37 +1,37 @@
-player_grid = [
-    [[2, 1], [2, 1], [2, 1], [2, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [5, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [5, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [5, 1], [0, 0], [0, 0], [6, 1], [6, 1], [6, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [5, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [5, 1], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-]
+#Imports the Point class from Classes
+from Classes import Point
+#Makes a empty grid
+def intialize_grid(Height = 11, Width = 11):
+    out_grid = []
+    for row_ind in range(Height):
+        row = []
+        for ind in range(Width):
+            row.append(Point())
+        out_grid.append(row)
+    return(out_grid)
+
+player_grid = intialize_grid()
 #This is the code for checking the grid to find fully destroyed ships.
 def destruction_check(grid):
     updated_grid = grid
     points = {}
     for rows in updated_grid:
         for pos in rows:
-            if pos[0] in points.keys():
-                points[pos[0]][0] += 1
-                if (pos[1]>0):
-                    points[pos[0]][1] += 1
+            if pos.get_ID() in points.keys():
+                points[pos.get_ID()][0] += 1
+                if (pos.get_State() > 0):
+                    points[pos.get_ID()][1] += 1
             else:
-                if (pos[1]>0):
-                    points[pos[0]] = [1, 1]
+                if (pos.get_State() > 0):
+                    points[pos.get_ID()] = [1, 1]
                 else:
-                    points[pos[0]] = [1, 0]
+                    points[pos.get_ID()] = [1, 0]
     for key in points:
         if points[key][0] == points[key][1]:
             for row_ind in range(0, len(updated_grid)):
                 for pos_ind in range(0, len(updated_grid[row_ind])):
-                    if updated_grid[row_ind][pos_ind][0] == key:
-                        updated_grid[row_ind][pos_ind][1] = -2
+                    if updated_grid[row_ind][pos_ind].get_ID() == key:
+                        updated_grid[row_ind][pos_ind].set_State(-2)
     return(updated_grid)
 #This is the code for firing at a point.
 def select_target(cord, grid):
@@ -39,17 +39,14 @@ def select_target(cord, grid):
     row = alphabet.index(cord[0 : 1])
     colum = int(cord[1 : len(cord)]) - 1
     updated_grid = grid
-    if updated_grid[row][colum][0] != 0:
-        if updated_grid[row][colum][1] == 0:
-            updated_grid[row][colum][1] = 1
-            return(updated_grid)
-        else:
-            return(updated_grid)
-    else:
-        return(updated_grid)
-player_grid = select_target("H10", player_grid)
-player_grid = destruction_check(player_grid)
+    print(updated_grid[row][colum].chosen_target())
+    return updated_grid
+    
 for row in player_grid:
     print(row)
-
-                
+while True:
+    choice = input("Please enter a cordinate: ")
+    player_grid = select_target(choice, player_grid)
+    player_grid = destruction_check(player_grid)
+    for row in player_grid:
+        print(row)  
