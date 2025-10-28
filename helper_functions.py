84a6d1b117037e087e818fd9c40a161ada605429
NotNotGrumm <trevor8700@icloud.com>
2025-10-28 10:51:04 -0500

    Added helper functions
    Add any misc or just generally helpful functions to helper_functions.py

---
+++ helper_functions.py
@@ -0,0 +1,7 @@
+def count_values(matrix, vals):
+    return [sum(row.count(val) for row in matrix) for val in vals]
+
+
+def translate_board(grid):
+    #Trevor: I'll use this to translate the way y'all implement board into a more UI generation friendly method
+    print("nah")
